a6116bcd8a291c57263a0d95a2cbfddc5db5d76c
Jhon Vebrianto Girsang <jvg.inq@gmail.com>
2026-08-05 13:58:43 +0700

    rename: sales-tracker -> leads-tracker, ubah label menu jadi Leads Tracker

--- navbar.js
+++ navbar.js
@@ -196,7 +196,7 @@ console.log('[Fawz navbar.js] v2.5.0 loaded');
       // Sales dropdown
       'sales-activity.html'    : ['head_account', 'admin', 'treasury', 'sales', 'head_sales'],
       'sales-transaction.html' : ['head_account', 'admin', 'treasury', 'sales', 'head_sales'],
-      'sales-tracker.html'     : ['head_account', 'admin', 'head_sales'],
+      'leads-tracker.html'     : ['head_account', 'admin', 'head_sales'],
       'sales.html'             : ['head_account', 'admin', 'head_sales'],
       'referral-agent.html'    : ['head_account', 'admin', 'head_sales'],
       'influencer.html'        : ['head_account', 'admin', 'head_sales'],
